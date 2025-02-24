2d0785a0bc040c8af26d487a21d82d3397f1b1b1
ELpath31 <span08goon@gmail.com>
2025-02-24 12:05:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const toggleButton = document.getElementById("toggleButton");
-const sidebar = document.getElementById("sidebar");
-const mainContent = document.getElementById("main-content");
 
-toggleButton.addEventListener("click", () => {
-    sidebar.classList.toggle("open");
-    mainContent.classList.toggle("open");
-});
